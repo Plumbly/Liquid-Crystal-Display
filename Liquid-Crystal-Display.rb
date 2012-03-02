8adf38567d000f036f9bd63252029d42fa170e05
Plumbly <josh.plumbly.09@aberdeen.ac.uk>
2012-03-02 16:42:15 +0000

    Have implemented display method, however there is a bug that throws an error when B is passed as an argument

--- Liquid-Crystal-Display.rb
+++ Liquid-Crystal-Display.rb
@@ -31,7 +31,7 @@ class LCD
        # as part of the data array.
        #
        @@lcdDisplayData = {
-	    "0" => [ 1, 3, 3, 3, 1 ],
+	    "0" => [ 1, 3, 0, 3, 1 ],
 	    "1" => [ 0, 1, 0, 1, 0 ],
 	    "2" => [ 1, 1, 1, 2, 1 ],
 	    "3" => [ 1, 1, 1, 1, 1 ],
@@ -63,18 +63,33 @@ class LCD
            @spacing = spacing
        end
 
-       def display( digits )
-            print digits
-	
-       end
-       
+       def display(characters)
+	      states = @@lcdStates  
+	      for index in 0...states.length
+		     pos = states[index]
+		     line = ""
+		     case pos
+		     when "HORIZONTAL"				       
+			    characters.each_byte do |a|			    
+				   line += horizontal_line(@@lcdDisplayData[a.chr][index])
+			    end		     
+		     when "VERTICAL"
+			    characters.each_byte do |b|
+				   line += vertical_line(@@lcdDisplayData[b.chr][index])					  
+			    end				   				   		     
+		     end
+		     print line + "\n"		     		    
+	      end	
+       end 
+             
        def horizontal_line(position)
 	   case position
 	   when 1	  
 	      line = " " + ("-" * @size) + " " + (" " * @spacing)
 	   when 0
 	      line = " " + (" " * @size) + " " + (" " * @spacing)
-	   end   
+	   end
+	   return line
        end
 	   
        def vertical_line(position)
@@ -87,10 +102,9 @@ class LCD
 	      line = "|" + (" " * @size) + "|" + (" " * @spacing)
            when 0
 	      line = " " + (" " * @size) + " " + (" " * @spacing)
-	   end  	      
+           end
+           return line
        end       
 end
-
-    d = LCD.new(1, 1)
-    d.display("12")
-    
+    d = LCD.new(2, 1)
+    d.display("0123456789ACDEF") 
